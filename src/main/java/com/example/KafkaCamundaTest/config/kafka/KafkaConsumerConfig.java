6daebbe7feb62434586c594fdecfeb6c13010652
revinsd <revinsd456@gmail.com>
2023-09-06 01:59:43 +0500

    fixed interaction with kafka

--- src/main/java/com/example/KafkaCamundaTest/config/kafka/KafkaConsumerConfig.java
+++ src/main/java/com/example/KafkaCamundaTest/config/kafka/KafkaConsumerConfig.java
@@ -2,13 +2,14 @@ package com.example.KafkaCamundaTest.config.kafka;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.deser.std.StringDeserializer;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 
@@ -33,6 +34,13 @@ public class KafkaConsumerConfig {
         config.put(KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         return new DefaultKafkaConsumerFactory<>(config);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory(){
+        var factory = new ConcurrentKafkaListenerContainerFactory<String, String>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
     }
 
     @Bean
--- src/main/java/com/example/KafkaCamundaTest/config/kafka/KafkaProducerConfig.java
+++ src/main/java/com/example/KafkaCamundaTest/config/kafka/KafkaProducerConfig.java
@@ -1,7 +1,6 @@
 package com.example.KafkaCamundaTest.config.kafka;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -13,9 +12,7 @@ import org.springframework.kafka.core.ProducerFactory;
 
 import java.util.HashMap;
 
-import static org.apache.kafka.clients.producer.ProducerConfig.BOOTSTRAP_SERVERS_CONFIG;
-import static org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG;
-import static org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.*;
 
 @Configuration
 @ConditionalOnProperty(name = "kafka.enabled", havingValue = "true")
